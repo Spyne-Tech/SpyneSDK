de5ffe417df8360f604a206e9dae5358f53c8339
Akash Verma <akash.verma@spyne.ai>
2023-01-24 15:08:52 +0530

    adding sdk

---
+++ Example/SpyneSDK.framework/Headers/SpyneSDK.h
@@ -0,0 +1,18 @@
+//
+//  SpyneSDK.h
+//  SpyneSDK
+//
+//  Created by Akash Verma on 27/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpyneSDK.
+FOUNDATION_EXPORT double SpyneSDKVersionNumber;
+
+//! Project version string for SpyneSDK.
+FOUNDATION_EXPORT const unsigned char SpyneSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpyneSDK/PublicHeader.h>
+
+
